1f5b27bc19549d082c2b19276ee7b359c7b1782e
Connor-Whiteside <connor.whiteside@my.jcu.edu.au>
2022-09-25 13:48:01 +1000

    Modified score program to include functions and docstrings

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,29 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+"""CP1404/CP5632 - Practical."""
 
-score = float(input("Enter score: "))
-if score <= 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Pass")
-else:
-    print("Excellent")
+import random
+
+
+def main():
+    """Score program."""
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+    random_score = random.randint(0, 100)
+    result = determine_result(random_score)
+    print(f"Your random score is {random_score}")
+    print(result)
+
+
+def determine_result(score):
+    """Determine a result then return a string based on score"""
+    if score <= 0:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Pass"
+    else:
+        return "Excellent"
+
+
+main()
