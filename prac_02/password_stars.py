6bd846560b9dc9ead24f7e700c9b7165424f4fcc
Connor-Whiteside <connor.whiteside@my.jcu.edu.au>
2022-09-25 12:50:40 +1000

    Added docstrings to program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,20 @@
-""""""
+"""Password Program."""
 
 
 def main():
+    """Password program."""
     minimum_password_length = 8
     password = get_password(minimum_password_length)
     display_asterisks(password)
 
 
 def display_asterisks(password):
+    """Print a line of length asterisks."""
     print("*" * len(password))
 
 
 def get_password(minimum_password_length):
+    """Return a password based on a set length."""
     password = input("Password: ")
     while len(password) < minimum_password_length:
         print("Password too short")
