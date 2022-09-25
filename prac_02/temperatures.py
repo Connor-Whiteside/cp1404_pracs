27adfcd3f3a170cdab55dd0b2031e1a1a615b7c6
Connor-Whiteside <connor.whiteside@my.jcu.edu.au>
2022-09-25 13:28:36 +1000

    Added docstrings to temperature program and main function

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -5,29 +5,36 @@ CP1404/CP5632 - Practical
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
 
-def calculate_celsius_to_fahrenheit():
+def calculate_celsius_to_fahrenheit(celsius):
+    """Calculates celsius to fahrenheit."""
     return celsius * 9.0 / 5 + 32
 
 
-def calculate_fahrenheit_to_celsius():
+def calculate_fahrenheit_to_celsius(fahrenheit):
+    """Calculates fahrenheit to celsius."""
     return 5 / 9 * (fahrenheit - 32)
 
 
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = calculate_celsius_to_fahrenheit()
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = calculate_fahrenheit_to_celsius()
-        print(f"Result: {celsius:.2f} F")
-    else:
-        print("Invalid option")
+def main():
+    """Temperature program."""
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = calculate_celsius_to_fahrenheit(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = calculate_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} F")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+main()
