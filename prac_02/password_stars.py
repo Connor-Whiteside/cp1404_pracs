32fe072990ee810f4b46c1224cd5bc057f9c3e5a
Connor-Whiteside <connor.whiteside@my.jcu.edu.au>
2022-09-25 12:40:28 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,22 @@
-MINIMUM_PASSWORD_LENGTH = 8
-password = input("Password: ")
-while len(password) < MINIMUM_PASSWORD_LENGTH:
-    print("Password too short")
+""""""
+
+
+def main():
+    minimum_password_length = 8
+    password = get_password(minimum_password_length)
+    display_asterisks(password)
+
+
+def display_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password(minimum_password_length):
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < minimum_password_length:
+        print("Password too short")
+        password = input("Password: ")
+    return password
+
+
+main()
