82ceaea7a834e20b1ad82a411e77218eb1b5690d
Connor-Whiteside <connor.whiteside@my.jcu.edu.au>
2022-10-18 09:41:56 +1000

    Add extra tasks to program state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,14 +4,19 @@ State names in a dictionary
 File needs reformatting
 """
 
-state_to_state_name = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(state_to_state_name)
+state_code_to_state_name = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory",
+                            "WA": "Western Australia",
+                            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(state_code_to_state_name)
 
-state = input("Enter short state: ").upper()
-while state != "":
-    if state in state_to_state_name:
-        print(state, "is", state_to_state_name[state])
-    else:
+is_valid_input = False
+while not is_valid_input:
+    try:
+        state = input("Enter short state: ").upper()
+        print(state, "is", state_code_to_state_name[state])
+        is_valid_input = True
+    except KeyError:
         print("Invalid short state")
-    state = input("Enter short state: ").upper()
+
+for state, full_name in state_code_to_state_name.items():
+    print(f"{state:3} is {full_name}")
