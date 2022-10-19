0c9841fb866d9cdffa1df6498a1201154b5a8af5
Connor-Whiteside <88589478+Connor-Whiteside@users.noreply.github.com>
2022-10-19 12:59:30 +1000

    Fix naming issues and dictonary naming conventions

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,10 +1,10 @@
-COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
-                "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
-                "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
-                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
-                "aquamarine4": "#458b74", "azure1": "#f0ffff",
-                "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
-                "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
+COLOUR_NAME_TO_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
+                        "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc",
+                        "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                        "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
+                        "aquamarine4": "#458b74", "azure1": "#f0ffff",
+                        "azure2": "#e0eeee", "azure3": "#c1cdcd", "azure4": "#838b8b",
+                        "beige": "#f5f5dc", "bisque1": "#ffe4c4"}
 
 # colour_name = input("Enter colour name: ")
 
@@ -13,7 +13,7 @@ while not is_valid_input:
     try:
         colour_name = input("Enter colour name: ")
         while colour_name != "":
-            print(f"{COLOUR_CODES[colour_name]}")
+            print(f"{COLOUR_NAME_TO_CODES[colour_name]}")
             colour_name = input("Enter colour name: ")
         is_valid_input = True
     except KeyError:
