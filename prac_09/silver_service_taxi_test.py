763e30b62d6cdcf5bb674bcc45b82af1f53400d1
Connor-Whiteside <connor.whiteside@my.jcu.edu.au>
2022-11-21 12:10:15 +1000

    Finish SilverServiceTaxi programs

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,14 @@
+"""CP1404 Silver Service Taxi Testing Prac 09."""
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Testing for Silver Service Taxi."""
+    s1 = SilverServiceTaxi("Gold Taxi", 100, 2)
+    print(s1)
+    s1.drive(18)
+    print(f"Total Cost: ${s1.get_fare()}")
+    print(s1)
+
+
+main()
